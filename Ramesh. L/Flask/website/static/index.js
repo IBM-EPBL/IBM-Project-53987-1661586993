e30827a75015b5d3b9a5a7c554fbab12b53dc195
naveennadal <114147646+naveennadal@users.noreply.github.com>
2022-09-29 20:00:57 +0530

    Delete Ramesh. L directory

--- Ramesh. L/Flask/website/static/index.js
+++
@@ -1,9 +0,0 @@
-@@ -0,0 +1,8 @@
-function deleteNote(noteId) {
-  fetch("/delete-note", {
-    method: "POST",
-    body: JSON.stringify({ noteId: noteId }),
-  }).then((_res) => {
-    window.location.href = "/";
-  });
-}
