f18cc3e657bbee9acddff9df63a987ba4490a404
kevinrosh <kevinroshand26@gmail.com>
2022-11-18 20:47:10 +0530

    Sprint-1,2&3

---
+++ Project Development Phase/Sprint 2/main.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, redirect, url_for
+from database import Database
+
+app = Flask(__name__, static_url_path='')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/validateRegister', methods=['GET', 'POST'])
+def validateRegister():
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+    db = Database()
+    db.creeateUser(name, email, password)
+    return render_template('login.html')
+
+
+@app.route('/validateLogin', methods=['GET', 'POST'])
+def validateLogin():
+    email = request.form['email']
+    password = request.form['password']
+    db = Database()
+    if password == db.loginUser(email)[0]:
+        return redirect(url_for('customercare'))
+    else:
+        return "Invalid Credentials"
+
+
+@app.route('/customercare')
+def customercare():
+    return render_template('customercare.html')
+
+if __name__ == "__main__":
+    app.run()
---
+++ Project Development Phase/Sprint 3/main.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, redirect, url_for
+from database import Database
+
+app = Flask(__name__, static_url_path='')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/validateRegister', methods=['GET', 'POST'])
+def validateRegister():
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+    db = Database()
+    db.creeateUser(name, email, password)
+    return render_template('login.html')
+
+
+@app.route('/validateLogin', methods=['GET', 'POST'])
+def validateLogin():
+    email = request.form['email']
+    password = request.form['password']
+    db = Database()
+    if password == db.loginUser(email)[0]:
+        return redirect(url_for('customercare'))
+    else:
+        return "Invalid Credentials"
+
+
+@app.route('/customercare')
+def customercare():
+    return render_template('customercare.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    email = request.form['email']
+    complaint = request.form['complaint']
+    db = Database()
+    db.registerComplaint(name, email, complaint)
+    return redirect(url_for('customercare'))
+    
+
+if __name__ == "__main__":
+    app.run()
